6222aa03a0c5082404b094635c8489206b90feb7
IvanGostev <ivangostev07@gmail.com>
2025-12-07 23:21:22 +0300

    update

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
+    @vite(['resources/css/base.css', 'resources/css/app.css',  'resources/js/app.js'])
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Мой профиль') }}
